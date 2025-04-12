eb9a7d75e8c72611e0b219982728fdea7b34931e
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-04-12 10:10:02 -0400

    external scope scenario

--- blackbox-multi-scope/src/main/java/org/multi/scope/ManyScope.java
+++ blackbox-multi-scope/src/main/java/org/multi/scope/ManyScope.java
@@ -3,10 +3,11 @@ package org.multi.scope;
 import org.multi.crosscut.CrossCutModule;
 import org.multi.modd.ModDModule;
 import org.multi.mode.ModEModule;
+import org.other.one.custom.*;
 
 import io.avaje.inject.InjectModule;
 import jakarta.inject.Scope;
 
 @Scope
-@InjectModule(requires = {ModDModule.class, CrossCutModule.class, ModEModule.class})
+@InjectModule(requires = {ExternalModule, ModDModule.class, CrossCutModule.class, ModEModule.class})
 public @interface ManyScope {}
